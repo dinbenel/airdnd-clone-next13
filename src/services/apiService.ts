0b1863eeb44ce01a7b0880ac2ea0fd316a36843f
Dinos <dinbenel@gmail.com>
2023-05-22 23:31:22 +0300

    url

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api"
+    : "https://airdnd-clone-next13-ks6m-4j6n2q12a-dinbenel.vercel.app/";
+
 export const http = axios.create({
-  baseURL: `http://localhost:3000/api`,
+  baseURL: BASE_URL,
   timeout: 60000,
   headers: {
     "Content-Type": "application/json",
