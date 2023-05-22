c637d9f177f2dc2774acc679b0c249e8f20dd11d
Dinos <dinbenel@gmail.com>
2023-05-22 23:02:54 +0300

    auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { getLogedInUser } from "@/utils/getLogedInUser";
 
 export default async function Home() {
   const logedInUserPrm = getLogedInUser();
-  const listingsPrm = getAllListing(); //
+  const listingsPrm = getAllListing();
 
   const [{ data: listings }, logedInUser] = await Promise.all([
     listingsPrm,
