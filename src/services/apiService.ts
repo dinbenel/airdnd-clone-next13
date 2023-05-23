23b284f25513733011ea127e4ef048ebfc2b1f0e
Din Ben Elisha <dinbenel@gmail.com>
2023-05-23 11:07:13 +0300

    api route fix

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api"
-    : "airdnd-clone-next13.vercel.app/api";
+    : "https://airdnd-clone-next13.vercel.app/api";
 
 export const http = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://airdnd-clone-next13.vercel.app/api",
   timeout: 60000,
   headers: {
     "Content-Type": "application/json",
